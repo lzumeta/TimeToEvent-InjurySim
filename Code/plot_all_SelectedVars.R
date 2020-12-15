ee1ecaa4ba74033205f52e7a6d3f5f88aa17867a
Lore Zumeta <lzumeta@DULX0054>
2020-12-15 15:36:00 +0100

    code and figure for SelectedVars files

--- Code/plot_all_SelectedVars.R
+++ Code/plot_all_SelectedVars.R
@@ -1,8 +1,13 @@
+library(gridExtra)
 
+source("Code/plot_selectedVars.R")
+
+ci <- TRUE
+CaseStudy <- FALSE
 
 ## Plot 1 -----------------
 ## load very small sample size, scenario 2
-dir <- "Codes/Simulations/Results/Scenario2/"
+dir <- "Results/Scenario2/"
 name <- "Nsim_100_Nobs_60_cens_0.75_xvars_50_verysmallsize"
 dir <- paste0(dir, name, "/")
 load(paste0(dir, "Data_", name, ".rds"))
@@ -13,7 +18,7 @@ load(paste0(dir, "VarsGroupLasso_", name, ".rds")); load(paste0(dir, "VarsCoxBoo
 p1 <- plot_selectedVars(ci = ci, CaseStudy = CaseStudy)
 
 ## load very small sample size, scenario 3
-dir <- "Codes/Simulations/Results/Scenario3/"
+dir <- "Results/Scenario3/"
 name <- "Nsim_100_Nobs_60_cens_0.75_xvars_50_verysmallsize"
 dir <- paste0(dir, name, "/")
 load(paste0(dir, "Data_", name, ".rds"))
@@ -24,13 +29,13 @@ load(paste0(dir, "VarsGroupLasso_", name, ".rds")); load(paste0(dir, "VarsCoxBoo
 p2 <- plot_selectedVars(ci = ci, CaseStudy = CaseStudy)
 
 p3 <- grid.arrange(p1, p2, ncol = 2)
-ggsave(plot = p3, filename = "Codes/Simulations/Results/SelectedVars_verysmallsize.pdf", width = 35, height = 12)
+ggsave(plot = p3, filename = "Figures/SelectedVars_verysmallsize.pdf", width = 35, height = 12)
 
 
 
 ## Plot 2 -----------------
 ## load small sample size, scenario 2
-dir <- "Codes/Simulations/Results/Scenario2/"
+dir <- "Results/Scenario2/"
 name <- "Nsim_100_Nobs_191_cens_0.75_xvars_50_smallsize"
 dir <- paste0(dir, name, "/")
 load(paste0(dir, "Data_", name, ".rds"))
@@ -41,7 +46,7 @@ load(paste0(dir, "VarsGroupLasso_", name, ".rds")); load(paste0(dir, "VarsCoxBoo
 p1 <- plot_selectedVars(ci = ci, CaseStudy = CaseStudy)
 
 ## load small sample size, scenario 3
-dir <- "Codes/Simulations/Results/Scenario3/"
+dir <- "Results/Scenario3/"
 name <- "Nsim_100_Nobs_191_cens_0.75_xvars_50_smallsize"
 dir <- paste0(dir, name, "/")
 load(paste0(dir, "Data_", name, ".rds"))
@@ -52,13 +57,13 @@ load(paste0(dir, "VarsGroupLasso_", name, ".rds")); load(paste0(dir, "VarsCoxBoo
 p2 <- plot_selectedVars(ci = ci, CaseStudy = CaseStudy)
 
 p3 <- grid.arrange(p1, p2, ncol = 2)
-ggsave(plot = p3, filename = "Codes/Simulations/Results/SelectedVars_smallsize.pdf", width = 35, height = 12)
+ggsave(plot = p3, filename = "Figures/SelectedVars_smallsize.pdf", width = 35, height = 12)
 
 
 
 ## Plot 3 -----------------
 ## load large sample size, scenario 2
-dir <- "Codes/Simulations/Results/Scenario2/"
+dir <- "Results/Scenario2/"
 name <- "Nsim_100_Nobs_391_cens_0.75_xvars_50_largesize"
 dir <- paste0(dir, name, "/")
 load(paste0(dir, "Data_", name, ".rds"))
@@ -69,7 +74,7 @@ load(paste0(dir, "VarsGroupLasso_", name, ".rds")); load(paste0(dir, "VarsCoxBoo
 p1 <- plot_selectedVars(ci = ci, CaseStudy = CaseStudy)
 
 ## load large sample size, scenario 2
-dir <- "Codes/Simulations/Results/Scenario3/"
+dir <- "Results/Scenario3/"
 name <- "Nsim_100_Nobs_391_cens_0.75_xvars_50_largesize"
 dir <- paste0(dir, name, "/")
 load(paste0(dir, "Data_", name, ".rds"))
@@ -80,13 +85,13 @@ load(paste0(dir, "VarsGroupLasso_", name, ".rds")); load(paste0(dir, "VarsCoxBoo
 p2 <- plot_selectedVars(ci = ci, CaseStudy = CaseStudy)
 
 p3 <- grid.arrange(p1, p2, ncol = 2)
-ggsave(plot = p3, filename = "Codes/Simulations/Results/SelectedVars_largesize.pdf", width = 35, height = 12)
+ggsave(plot = p3, filename = "Figures/SelectedVars_largesize.pdf", width = 35, height = 12)
 
 
 
 ## Plot 4 -----------------
 ## load large sample size, scenario 2
-dir <- "Codes/Simulations/Results/Scenario2/"
+dir <- "Results/Scenario2/"
 name <- "Nsim_100_Nobs_670_cens_0.75_xvars_50_verylargesize"
 dir <- paste0(dir, name, "/")
 load(paste0(dir, "Data_", name, ".rds"))
@@ -97,7 +102,7 @@ load(paste0(dir, "VarsGroupLasso_", name, ".rds")); load(paste0(dir, "VarsCoxBoo
 p1 <- plot_selectedVars(ci = ci, CaseStudy = CaseStudy)
 
 ## load very small sample size, scenario 2
-dir <- "Codes/Simulations/Results/Scenario3/"
+dir <- "Results/Scenario3/"
 name <- "Nsim_100_Nobs_670_cens_0.75_xvars_50_verylargesize"
 dir <- paste0(dir, name, "/")
 load(paste0(dir, "Data_", name, ".rds"))
@@ -108,4 +113,7 @@ load(paste0(dir, "VarsGroupLasso_", name, ".rds")); load(paste0(dir, "VarsCoxBoo
 p2 <- plot_selectedVars(ci = ci, CaseStudy = CaseStudy)
 
 p3 <- grid.arrange(p1, p2, ncol = 2)
-ggsave(plot = p3, filename = "Codes/Simulations/Results/SelectedVars_verylargesize.pdf", width = 35, height = 12)
+ggsave(plot = p3, filename = "Figures/SelectedVars_verylargesize.pdf", width = 35, height = 12)
+
+
+
--- Code/plot_selectedVars.R
+++ Code/plot_selectedVars.R
@@ -1,6 +1,10 @@
+library(magrittr)
+library(tidyverse)
+library(ggnewscale) ## new_scale_fill()
+
 plot_selectedVars <- function(ci = FALSE, CaseStudy = FALSE) {
   
-  ## How many non-zero variables are detected?
+  ## How many non-zero variables are detected? 
   vars.bess.nonzero  <- apply(coefs.bess,  1, function(x) x != 0) %>% rowMeans()
   vars.lasso.nonzero <- apply(coefs.lasso, 1, function(x) x != 0) %>% rowMeans()
   vars.ridge.nonzero <- apply(coefs.ridge, 1, function(x) x != 0) %>% rowMeans()
@@ -10,27 +14,30 @@ plot_selectedVars <- function(ci = FALSE, CaseStudy = FALSE) {
     vars.ridge.nonzero <- vars.ridge2 %>% unlist() %>% table()/Nsim %>% as.vector() 
     vars.ridge.nonzero <- vars.ridge.nonzero[paste0("X", 1:xvars)] ## order
   }
-  ##
   
   vars.grouplasso.nonzero <- apply(coefs.grouplasso, 1, function(x) x != 0) %>% rowMeans()
-  vars.coxboost.nonzero <- apply(coefs.coxboost, 1, function(x) x != 0) %>% rowMeans()
+  vars.coxboost.nonzero   <- apply(coefs.coxboost, 1, function(x) x != 0) %>% rowMeans()
+  ##
   
-  
+  ## Create table
   mat_selected <- t(rbind(vars.bess.nonzero, vars.lasso.nonzero, vars.enet.nonzero, vars.ridge.nonzero,
                           vars.grouplasso.nonzero, vars.coxboost.nonzero))[,6:1]
   if(CaseStudy) {
     labels <- names(data)[9:ncol(data)]
+    nrelev_vars <- ifelse(var_method == "BESS", 2, 
+                          ifelse(var_method == "LASSO", 6, 4)
+                          )
   } else {
     labels <- paste0("X", 1:xvars)
+    nrelev_vars <- 5
   }
   
   upmargin <- ifelse(CaseStudy, 10, 5)
-
   
   ## New plot
   mat_selected2 <- as.data.frame(mat_selected)
-  mat_selected2$type <- c(rep("relevant", 5), rep("nonrelevant", 45))
-  mat_selected2$vars <- paste0("X", 1:50)
+  mat_selected2$type <- c(rep("relevant", nrelev_vars), rep("nonrelevant", xvars-nrelev_vars))
+  mat_selected2$vars <- paste0("X", 1:xvars)
   
   mat_selected2 <- gather(mat_selected2, key = selection, value = value, -type, -vars) %>% 
     mutate(type = factor(type, levels = c("relevant", "nonrelevant")),
@@ -38,15 +45,15 @@ plot_selectedVars <- function(ci = FALSE, CaseStudy = FALSE) {
                               levels = c("vars.coxboost.nonzero", "vars.grouplasso.nonzero", "vars.ridge.nonzero", 
                                          "vars.enet.nonzero", "vars.lasso.nonzero", "vars.bess.nonzero"), 
                               label = c("Boosting", "Group Lasso", "Ridge", "Elastic Net", "Lasso", "BeSS")),
-           vars = factor(vars, levels = paste0("X", 1:50)))
+           vars = factor(vars, levels = paste0("X", 1:xvars)))
   
   mat_selected2_rev <- mat_selected2[mat_selected2$type == "relevant",]
   mat_selected2_nonrev <- mat_selected2[mat_selected2$type == "nonrelevant",]
   
   ggplot(data = mat_selected2, aes(y = selection, x = vars)) +
-   geom_tile(colour="white", size=0.7) + 
+    geom_tile(colour="white", size=0.7) + 
     geom_tile(dat = mat_selected2_rev, aes(fill= value), colour="white", size=0.7) +
-    scale_fill_gradient(low = "white" ,high = "blue", name = "Frequency of selected\nrelevant variables",
+    scale_fill_gradient(low = "white", high = "blue", name = "Frequency of selected\nrelevant variables",
                         breaks = seq(0, 1, length.out = 11), limits = c(0,1)) +
     new_scale_fill()+ 
     geom_tile(dat = mat_selected2_nonrev, aes(fill= value), colour="white", size=0.7) +
@@ -54,14 +61,14 @@ plot_selectedVars <- function(ci = FALSE, CaseStudy = FALSE) {
                         breaks = seq(0, 1, length.out = 11), limits = c(0,1)) +
     xlab("") + ylab("") +
     scale_x_discrete(position = "top") +
+    coord_cartesian(expand=FALSE)  +
     theme_bw() +
     theme(panel.grid=element_blank(),
           axis.text.x = element_text(size = 10),
           axis.text.y = element_text(size = 22),
           legend.text = element_text(size = 16),
           legend.title = element_text(size = 20),
-          legend.key.size = unit(3.5, "lines")) +
-    coord_cartesian(expand=FALSE) 
-
+          legend.key.size = unit(3.5, "lines")) 
+  
 } 
 
